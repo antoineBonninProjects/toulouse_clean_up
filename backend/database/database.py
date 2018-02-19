b1a50bc24b5b30a23dc8137e1e6a05df53041475
Antoine Bonnin <bonnin.annto@gmail.com>
2018-02-19 19:06:59 +0100

    Creation of a rest_apis module
    REST_APIS module
    modified: rest_apis/data_receiver_api.py - data_receiver.py now belongs to the rest_api module
    modified: rest_apis/__init__.py - the module is no longer data_receiver but rest_apis
    added: rest_apis/data_receiver_api.py - new HTTP GET API service to get the content of latest_sigfox_messages table (JSON formatted) (/api/get_latest_sigfox_messages)
    added: rest_apis/__init__.py - FlaskAppLauncher class that starts the Flask server that we use for all our HTTP services
    added: rest_api/database_reader_api.py - new file for HTTP services that will be called by our Angular2 WebApp to read values inside the database.
    added: rest_api/database_reader_api.py - new HTTP GET API service to return the latest communicated weight (JSON formatted) for a given device (/api/get_sigfox_messages/latest/weight/<device_id>)
    
    DATABASE module
    added: database/database.py - LatestSigFoxMessages class that represents the latest_sigfox_message database table: where we store the latest message data for each SigFox devicei
    
    modified: backend_main.py - runs the FlaskAppLauncher and instantiate the DataReceiver and DatabaseReader objects

--- backend/database/database.py
+++ backend/database/database.py
@@ -15,9 +15,10 @@ Session = sessionmaker(bind=engine)
 The session object to interract with our database.
 """
 
+
 class SigFoxMessages(Base):
     """
-    A class that represents the table storing the payload of incoming SigFox messages.
+    A class that describes the schema of the table storing the payload of incoming SigFox messages.
 
     Attributes:
         id (Column): the message ID, it is the primary key of the table "sigfox_messages"
@@ -35,13 +36,45 @@ class SigFoxMessages(Base):
     @property
     def serialize(self):
         """
-        Returns object data in easily serializeable format
+        Returns object data in easily serializable format
 
         Returns:
             dict: the serialized representation of the SigFoxMessages class attributes.
         """
         return {
             'id': self.id,
+            'device_id': self.device_id,
+            'seq_num': self.seq_num,
+            'weight': self.weight
+        }
+
+
+class LatestSigFoxMessages(Base):
+    """
+    A class that describes the schema of the table storing the payload of the latest SigFox messages that has been
+    received for each devices.
+    This table is here to make our db queries faster.
+
+    Attributes:
+        device_id (Column): the ID of the SigFox message that has sent the frame
+        seq_num (Column): the sequence number of the message to store for the given SigFox device.
+        weight (Column): the weight that has been uplinked by the SigFox device.
+    """
+    __tablename__= "latest_sigfox_messages"
+
+    device_id = Column("device_id", String, primary_key=True)
+    seq_num = Column("seq#", Integer)
+    weight = Column("weight", Integer)
+
+    @property
+    def serialize(self):
+        """
+        Returns object data in easily serializable format
+
+        Returns:
+            dict: the serialized representation of the LatestSigFoxMessages class attributes.
+        """
+        return {
             'device_id': self.device_id,
             'seq_num': self.seq_num,
             'weight': self.weight
